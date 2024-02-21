58a1673ef70a2cb16be50477635d8cb10fad6d83
Patrick Reis <patrickbernardor@gmail.com>
2024-02-21 10:58:25 -0300

    chore: add initial migration

--- Migrations/20240219132042_InitialMigrations.Designer.cs
+++ Migrations/20240219132042_InitialMigrations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Oasis.Context;
@@ -11,9 +12,11 @@ using Oasis.Context;
 namespace Oasis.Migrations
 {
     [DbContext(typeof(EntityContext))]
-    partial class EntityContextModelSnapshot : ModelSnapshot
+    [Migration("20240219132042_InitialMigrations")]
+    partial class InitialMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
