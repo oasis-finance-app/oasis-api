698247ae1d932b9fb03eeeb6bac0123e5d8ac9ac
Patrick Reis <patrickbernardor@gmail.com>
2024-02-21 11:07:20 -0300

    refactor: move banks names from enum to db

---
+++ DTOs/BankAccountDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Oasis.Models;
+
+namespace Oasis.DTOs;
+
+
+public class BankAccountDto
+{
+	[Key]
+	public int BankAccountId { get; set; }
+
+	[Required]
+	public string AccountName { get; set; }
+
+	[Required]
+	public int BankId { get; set; }
+	public Bank? Bank { get; set; }
+
+	public string? OtherBankName { get; set; }
+
+}
